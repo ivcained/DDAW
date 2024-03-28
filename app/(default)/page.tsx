4b77f967820a313bad992fc804a3381719f958a6
ivcained <86070833+ivcained@users.noreply.github.com>
2024-03-28 11:09:25 -0600

    chore: landing edits

---
+++ app/(default)/page.tsx
@@ -0,0 +1,22 @@
+export const metadata = {
+  title: "DDAW.XYZ - The Decentralized DAW",
+  description: "Record, Play, Mix, Master, Mint Audio Video NFTs",
+};
+
+import Hero from "@/components/hero";
+import Features from "@/components/features";
+import Newsletter from "@/components/newsletter";
+import Zigzag from "@/components/zigzag";
+import Testimonials from "@/components/testimonials";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <Zigzag />
+      {/* <Testimonials /> */}
+      <Newsletter />
+    </>
+  );
+}
---
+++ components/zigzag.tsx
@@ -0,0 +1,242 @@
+import Image from "next/image";
+
+import FeatImage01 from "@/public/images/features-03-image-01.png";
+import FeatImage02 from "@/public/images/features-03-image-02.png";
+import FeatImage03 from "@/public/images/features-03-image-03.png";
+
+export default function Zigzag() {
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="py-12 md:py-20 border-t border-gray-800">
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+            <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">
+              No More Middlemen
+            </div>
+            <h1 className="h2 mb-4">
+              Buy & Sell Samples, Beats, Arrangements on Chain
+            </h1>
+            <p className="text-xl text-gray-400">
+              On mint, the DDAW will generate a unique NFT by making a mix down
+              and adjusting the Channel Levels and FX Sends and a Master Bus
+              Channel based on the time of mint and wallet address autonomously
+              until 512 NFTs are Minted on Base (OPStack, L2) and Ethereum (L1)
+            </p>
+          </div>
+
+          {/* Items */}
+          <div className="grid gap-20">
+            {/* 1st item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1"
+                data-aos="fade-up"
+              >
+                <Image
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src={FeatImage01}
+                  width={540}
+                  height={405}
+                  alt="Features 01"
+                />
+              </div>
+              {/* Content */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+                data-aos="fade-right"
+              >
+                <div className="md:pr-4 lg:pr-12 xl:pr-16">
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">
+                    A Revolution in Songs, Beats, Sample Production Technology.
+                  </div>
+                  <h3 className="h3 mb-3">Music Studio On Chain</h3>
+                  <p className="text-xl text-gray-400 mb-4">
+                    The tracks will be put individually on our own custom
+                    version of Helicon: forked from tr666eth/helicon A library
+                    for making music with web audio. It was developed as part of
+                    the audioglyphs project, but we hope that it will be useful
+                    to others working on similar things.
+                  </p>
+                  <ul className="text-lg text-gray-400 -mb-2">
+                    <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Works on majority Browsers</span>
+                    </li>
+                    <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Lossless Audio Processing</span>
+                    </li>
+                    <li className="flex items-center">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>
+                        FX, One Button Remix, Mint straight from the DAW!
+                      </span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            {/* 2nd item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl"
+                data-aos="fade-up"
+              >
+                <Image
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src={FeatImage02}
+                  width={540}
+                  height={405}
+                  alt="Features 02"
+                />
+              </div>
+              {/* Content */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+                data-aos="fade-left"
+              >
+                <div className="md:pl-4 lg:pl-12 xl:pl-16">
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">
+                    Web Audio API is in every modern Browser.
+                  </div>
+                  <h3 className="h3 mb-3">Keep projects on schedule</h3>
+                  <p className="text-xl text-gray-400 mb-4">
+                    It aims to use native web audio APIs as directly as possible
+                    and to be general purpose - avoiding including any specific
+                    audio processing or sequencing code in favor of things that
+                    apply to any project and any style of music.
+                  </p>
+                  <ul className="text-lg text-gray-400 -mb-2">
+                    <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Program your royalties in code (NFTs)</span>
+                    </li>
+                    <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>
+                        Create instant Visualization of Sound and Mint A/V NFTs
+                      </span>
+                    </li>
+                    <li className="flex items-center">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>
+                        Control every Channel and modulate FX, Sends and Level
+                        Automation or let the DDAW AI decide or ask it for a
+                        style.
+                      </span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            {/* 3rd item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1"
+                data-aos="fade-up"
+              >
+                <Image
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src={FeatImage03}
+                  width={540}
+                  height={405}
+                  alt="Features 03"
+                />
+              </div>
+              {/* Content */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+                data-aos="fade-right"
+              >
+                <div className="md:pr-4 lg:pr-12 xl:pr-16">
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">
+                    Fully Open Source
+                  </div>
+                  <h3 className="h3 mb-3">Keep projects on schedule</h3>
+                  <p className="text-xl text-gray-400 mb-4">
+                    Anyone can submit feedback, use the code, add to the code,
+                    make proposals to the DAO.
+                  </p>
+                  <ul className="text-lg text-gray-400 -mb-2">
+                    <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>https://warpcast.com/~/channel/ddaw</span>
+                    </li>
+                    <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>https://github.com/ivcained/DDAW</span>
+                    </li>
+                    <li className="flex items-center">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>hi@ddaw.xyz</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
